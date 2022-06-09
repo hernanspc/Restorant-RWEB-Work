bcdf0a7f0065dc16e7e4a0da55b3cfd2bf81e0e3
Hernan Pormachi <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-09 18:19:16 -0500

    add react router dom react-router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 
 import Ordenes from "./components/pages/Ordenes";
+import Menu from "./components/pages/Menu";
+import NuevoPlatillo from "./components/pages/NuevoPlatillo";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Ordenes />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
       </Routes>
     </div>
   );
