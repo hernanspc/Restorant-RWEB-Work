7947f3ca1189c5aebd749e0c8b7f7de34046d4fc
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-15 01:07:04 -0500

    Updates

---
+++ precache-manifest.2b839af25f7d2f98d5e9c478bb58076a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaf3b460b5da847c2bbcb2e5973aaa1",
+    "url": "/Restorant-RWEB-Work/index.html"
+  },
+  {
+    "revision": "8e1d8b2a61dd795ce426",
+    "url": "/Restorant-RWEB-Work/static/css/main.3bf56657.chunk.css"
+  },
+  {
+    "revision": "1657be8ee21d0450b02e",
+    "url": "/Restorant-RWEB-Work/static/js/2.1d45e8ec.chunk.js"
+  },
+  {
+    "revision": "145c9c1172a9beda451de4400565c985",
+    "url": "/Restorant-RWEB-Work/static/js/2.1d45e8ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1d8b2a61dd795ce426",
+    "url": "/Restorant-RWEB-Work/static/js/main.0a8e3845.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Restorant-RWEB-Work/static/js/main.0a8e3845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be9ee5d9c0d01d74e5d0",
+    "url": "/Restorant-RWEB-Work/static/js/runtime-main.9367eeaf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Restorant-RWEB-Work/precache-manifest.2b839af25f7d2f98d5e9c478bb58076a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Restorant-RWEB-Work/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
